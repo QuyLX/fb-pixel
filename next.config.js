4e967e7c1202a44f045c827bf6b8aa3d1b66d707
quylx <quylx@omegatheme.com>
2022-05-21 18:37:18 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
+const withCSS = require('@zeit/next-css');
+const withPurgeCss = require('next-purgecss');
+const withSass = require('@zeit/next-sass');
 
-module.exports = nextConfig
+const miniCss = withCSS(
+  withPurgeCss({
+    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+  })
+);
+
+const cssModules = withSass({
+  cssModules: false,
+});
+
+module.exports = {
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  cssModules,
+  miniCss,
+  env: {},
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+  image: {
+    formats: ['image/png'],
+  },
+};
